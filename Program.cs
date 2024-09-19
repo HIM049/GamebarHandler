6ddd1c50fdfd45680a74f2f9fbc2471cd0956dc7
Aida Enna <Aida-Enna@users.noreply.github.com>
2024-09-19 06:23:26 -0400

    Initial commit, just need to wait for windows to randomly try to open the gamebar so I can test it

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+
+namespace GamebarHandler
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("Handled the Gamebar stuff!");
+                Console.WriteLine("Press any key to exit the program...");
+                Console.ReadKey();
+            }
+            else
+            {
+                try
+                {
+                    using (RegistryKey newRegKey = Registry.ClassesRoot.CreateSubKey("ms-gamebar", RegistryKeyPermissionCheck.ReadWriteSubTree))
+                    {
+                        newRegKey.SetValue("", "URL:GamebarHandler");
+                        newRegKey.SetValue("URL Protocol", "");
+
+                        using (RegistryKey newRegCom = newRegKey.CreateSubKey(@"shell\open\command"))
+                        {
+                            string ProgramPath = Process.GetCurrentProcess().MainModule.FileName;
+                            newRegCom.SetValue("", $"\"{ProgramPath}\" called-from-uri");
+                        }
+                    }
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"Saved registry values for ms-gamebar URI to open {Process.GetCurrentProcess().MainModule.FileName}.");
+                    Console.ResetColor();
+                    Console.WriteLine("Whenever windows tries to open the Gamebar now, it should open this program instead, which will then silently close.");
+                    Console.WriteLine("Press any key to exit the program...");
+                    Console.ReadKey();
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    switch (ex.GetType().ToString())
+                    {
+                        case "System.UnauthorizedAccessException":
+                            Console.WriteLine("Access denied - Please make sure you are running this program as administrator!");
+                            break;
+                        default:
+                            Console.WriteLine("Something went wrong:" + Environment.NewLine + ex);
+                            break;
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("Press any key to exit the program...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GamebarHandler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GamebarHandler")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b83581ad-0f82-46c3-a1f0-e6b1b5d0d069")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
